5e75cbe4fc6dcaf689e120e3720c07e58b40616b
Joseph Sameh Fouad <148119993+Joseph-Sameh-0@users.noreply.github.com>
2025-03-13 18:37:42 +0200

    Update assignment1.pl

---
+++ src/assignment1.pl
@@ -0,0 +1,17 @@
+:- consult('../data/league_data').
+
+% TASK 1: Get a list of all players in a specific team
+
+% TASK 2: Count how many teams are from a specific country
+
+% TASK 3: Find the team with the most championship titles
+
+% TASK 4: List all matches where a specific team participated
+
+% TASK 5: Count all matches where a specific team participated
+
+% TASK 6: Find the top goal scorer in the tournament
+
+% TASK 7: Find the most common position in a specific team
+
+% Bonus Task: Control Unneeded backtracking in all predicates 
