8ee498e310e4fcb3ec7003414427345e88ad6913
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-03-16 18:14:28 +0200

    Update Task1.pl

--- src/Rana Ibrahim 20220130/Task1.pl
+++ src/Rana Ibrahim 20220130/Task1.pl
@@ -1,17 +1,13 @@
 :- consult('../../data/league_data.pl').
+:- consult('../helperFunctions.pl').
 % TASK 1: Get a list of all players in a specific team.
-%base case%
-is_member(X,[X|_]):- !.
-%recursive case%
-is_member(X,[_|T]):-
-    is_member(X,T).
 
 %base case%
 my_append([],L2,L2).
 %recursive case%
 my_append([X|T],L2,[X|Result]):-
     my_append(T,L2,Result).
-    
+
 
 %interface%
 players_in_team(Team, L):-
@@ -20,7 +16,7 @@ players_in_team(Team, L):-
 %recursive case%
 players_in_team(Team, List, Result):-
     player(X, Team, _),
-    \+ is_member(X,List),
+    \+ my_member(X,List),
     my_append([X],List,NewList),
     !,
     players_in_team(Team, NewList, Result).
